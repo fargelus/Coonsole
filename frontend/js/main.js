9210e8bac98f42c495d19f63c200dac66115800a
fargelus <ddraudred@gmail.com>
2018-08-08 09:29:03 +0300

    Добавил загрузку картинок в проект

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,20 @@
 import Vue from 'vue';
-import App from './vue/App.vue';
+import App from '../vue/App.vue';
 import '../styles/all.css';
+import LogoIcon from '../assets/images/coonsole_logo.svg';
+import CompassIcon from '../assets/images/compass.svg';
+
+// Привязывает путь к картинкам в бандле
+function bindImagesSource(imagesObj) {
+  Object.keys(imagesObj).forEach((selector) => {
+    const imagePath = imagesObj[selector];
+    const HTMLCollection = document.querySelectorAll(selector);
+
+    HTMLCollection.forEach((singleElem) => {
+      singleElem.src = imagePath;
+    });
+  });
+}
 
 const vm = new Vue({
   el: '#app',
@@ -10,3 +24,10 @@ const vm = new Vue({
 
   render: h => h(App),
 });
+
+const DOMSelectorToImagesMap = {
+  '.js-logo': LogoIcon,
+  '.js-compass': CompassIcon,
+};
+
+bindImagesSource(DOMSelectorToImagesMap);
---
+++ frontend/vue/App.vue
@@ -0,0 +1,16 @@
+<template id="js-header" class="header-top">
+    <div class="header-wrapper">
+      <img class="js-logo" alt="coonsole_logo" width="54" height="48">
+
+      <div class="site-search">
+        <input type="text" class="site-search__input" placeholder="Введите запрос">
+        <button type="button" class="site-search__button">Найти</button>
+      </div>
+
+      <!-- Пока поставим заглушки -->
+      <button type="button" class="user-location">
+        <!-- TODO CSS -->
+        <img class="js-compass">Санкт-Петербург
+      </button>
+    </div>
+</template>
