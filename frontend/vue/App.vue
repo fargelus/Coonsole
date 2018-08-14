6cd419bc32752770040cdcbf3961d2d3131e1954
fiCeVitka <gmac.net.ru@gmail.com>
2018-08-15 01:45:25 +0300

    Стили для шапки, мб коряво, но хоть как-то :/

--- frontend/vue/App.vue
+++ frontend/vue/App.vue
@@ -1,8 +1,29 @@
 <template>
-  <div id="app">
-    <Header/>
+  <div id="wrapper">
+    <div id="app">
+      <Header/>
+    </div>
   </div>
 </template>
+
+<style>
+  #wrapper {
+    border-top: 5px solid #EC5A24;
+    width: 100%;
+    padding: 8px 0;
+  }
+
+  #app {
+    max-width: 940px;
+    margin: 0 auto;
+    padding: 0;
+  }
+
+  .top_line {
+    width: 100%;
+    height: 4px;
+  }
+</style>
 
 <script>
   import Header from './components/header.vue';
--- frontend/vue/components/header.vue
+++ frontend/vue/components/header.vue
@@ -4,12 +4,12 @@
       <img class="js-logo" alt="coonsole_logo" width="54" height="48">
 
       <div class="site-search header-top__search interact-element">
-        <input type="text" class="site-search__input" :placeholder="Search_enter">
-        <button type="button" class="button site-search__button">{{ Find }}</button>
+        <input type="text" class="site-search__input interact-element" :placeholder="Search_enter">
+        <button type="button" class="button site-search__button interact-element">{{ Find }}</button>
       </div>
 
       <!-- Пока поставим заглушки -->
-      <button type="button" class="button interact-element transparent header-top__button">
+      <button type="button" class="button interact-element site-city__button transparent header-top__button">
         <!-- TODO CSS -->
         <img class="js-compass">{{ City }}
       </button>
@@ -22,22 +22,9 @@
   </div>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        Sale: 'Продать',
-        Sign_In: 'Войти',
-        City: 'Санкт-Петербург',
-        Find: 'Найти',
-        Search_enter: 'Введите запрос'
-      };
-    }
-  }
-</script>
-
 <style scoped>
   .header-top {
+    font-size: 16px;
     align-items: center;
   }
 
@@ -53,4 +40,64 @@
   .header-top__button {
     margin-left: 20px;
   }
+
+  .site-search {
+    position: relative;
+    width: 280px;
+    margin-top: 8px;
+  }
+
+  input.site-search__input {
+    position: absolute;
+    left: 0;
+    line-height: 32px;
+    color: #45413F;
+    outline: 0;
+    background-color: #EDEDED;
+    border: 0;
+    padding: 0 10px;
+    box-sizing: border-box;
+    margin: 0;
+    width: calc(100% - 73px);
+
+    -webkit-border-top-left-radius: 4px;
+    -webkit-border-bottom-left-radius: 4px;
+    -moz-border-radius-topleft: 4px;
+    -moz-border-radius-bottomleft: 4px;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+
+  .site-search__button {
+    position: absolute;
+    right: 0;
+    background-color: #EDEDED;
+    border: 0;
+    border-radius: 0;
+
+    -webkit-border-top-right-radius: 4px;
+    -webkit-border-bottom-right-radius: 4px;
+    -moz-border-radius-topright: 4px;
+    -moz-border-radius-bottomright: 4px;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
+
+  .site-city__button {
+    margin-top: 8px;
+  }
 </style>
+
+<script>
+  export default {
+    data() {
+      return {
+        Sale: 'Продать',
+        Sign_In: 'Войти',
+        City: 'Санкт-Петербург',
+        Find: 'Найти',
+        Search_enter: 'Введите запрос'
+      };
+    }
+  }
+</script>
