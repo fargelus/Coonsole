2381ea4b679650f3457e2929741f63c43ec98dc2
fargelus <ddraudred@gmail.com>
2018-09-02 16:21:45 +0300

    Api geonames

--- frontend/vue/components/CitiesListModal.vue
+++ frontend/vue/components/CitiesListModal.vue
@@ -4,11 +4,33 @@
       <input type="text" name="" value="" placeholder="Введите город">
     </div>
 
-    
+
   </div>
 </template>
 
 <script>
+  const serviceURL = 'http://api.geonames.org';
+  const username = 'fargelus';
+  let method = 'countryInfo';
+
+  const countryInfoURL = `${serviceURL}/${method}?lang=ru&country=RU&username=${username}`;
+  const coords = {};
+
+  // Получим координаты РОССИИ
+  $.get(countryInfoURL, (xmlData) => {
+    coords.west = $(xmlData).find('west').text();
+    coords.east = $(xmlData).find('east').text();
+    coords.north = $(xmlData).find('north').text();
+    coords.south = $(xmlData).find('south').text();
+  }).then(() => {
+    method = 'citiesJSON';
+    const citiesListURL = `${serviceURL}/?north=${coords.north}&south=${coords.south}&east=${coords.east}&west=${coords.west}&lang=ru&username=${username}`;
+
+    $.getJSON(citiesListURL, (citiesList) => {
+      console.dir(citiesList);
+    });
+  });
+
   export default {
     data() {
       return {
