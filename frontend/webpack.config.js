f0cbb0da4d429fd9721bc6ef9c280fd341d92c86
fargelus <ddraudred@gmail.com>
2019-01-13 14:36:18 +0300

    Компоненты Vue в твиге

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,8 +3,16 @@ const Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('../public/')
     .setPublicPath('/')
-    .enableSingleRuntimeChunk()
-    .addEntry('bundle', './test.js');
+    .addEntry('bundle', './ts/main.ts')
+    .disableSingleRuntimeChunk()
+    .addLoader({
+        test: /\.styl$/,
+        loader: 'vue-style-loader!css-loader!stylus-loader',
+    })
+    .enableVueLoader()
+    .enableTypeScriptLoader((tsconfig) => {
+        tsconfig.configFile = __dirname + '/tsconfig.json';
+    });
 
 module.exports = Encore.getWebpackConfig();
 
